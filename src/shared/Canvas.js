cd05e47e74a834ba0907921045bdf67b19b33787
oshi97 <oys4cv@virginia.edu>
2019-01-10 19:42:39 -0500

    separated animation from react component
    having the context tied to the react component had some weird bugs with resizing and probably didn't really have any point as far as I can see so far

--- src/shared/Canvas.js
+++ src/shared/Canvas.js
@@ -13,21 +13,52 @@ class Circle {
   }
 }
 
-var x = 200
 let ctx = null
 let canvas = null
 let animationFrameId = null
+let innerWidth = null
+let innerHeight = null
+
+let dx = 10
+let dy = 10
+let xScalar = 1
+let yScalar = 1
+let x = 200
+let y = 200
+let radius = 30
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.beginPath()
+  ctx.arc(x, y, radius, 0, Math.PI*2, false)
+  ctx.strokeStyle = 'blue'
+  ctx.stroke()
+  // parenthesis not needed but for clarity
+  if (x+radius > innerWidth) {
+    xScalar = -1
+  } else if (x-radius < 0){
+    xScalar = 1
+  }
+  if (y+radius > innerHeight) {
+    yScalar = -1
+  } else if (y-radius < 0){
+    yScalar = 1
+  }
+  x += xScalar*(dx+Math.random()-0.5)
+  y += yScalar*(dy+Math.random()-0.5)
+  animationFrameId=requestAnimationFrame(animate)
+}
 
 class Canvas extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { width: 0, height: 0 }
+
     this.updateDimensions = this.updateDimensions.bind(this) 
     this.handleClick = this.handleClick.bind(this)
-    this.animate = this.animate.bind(this)
   }
   componentDidMount() {
     this.updateDimensions()
+    this.loadCanvas()
     window.addEventListener("resize", this.updateDimensions)
   }
   componentWillUnmount() {
@@ -35,40 +66,35 @@ class Canvas extends React.Component {
     cancelAnimationFrame(animationFrameId)
   }
   componentDidUpdate() {
-    this.loadCanvas()
+
   }
   updateDimensions() {
-    this.setState({ width:window.innerWidth-8, height:window.innerHeight-8})
-  }
-  animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.beginPath()
-    ctx.arc(x, 200, 30, 0, Math.PI*2, false)
-    ctx.strokeStyle = 'blue'
-    ctx.stroke()
-    x += 1
-    console.log(x)
-    animationFrameId= requestAnimationFrame(this.animate)
+    console.log("UPDATE")
+    innerHeight = window.innerHeight
+    innerWidth = window.innerWidth
+    this.refs.canvas.height = window.innerHeight
+    this.refs.canvas.width = window.innerWidth
   }
   loadCanvas() {
     if (!this.refs.canvas) { console.log("> Canvas not found for loadCanvas()") }
     canvas = this.refs.canvas
     ctx = canvas.getContext('2d')
-    canvas.style.border = '4px solid #000000'
-    this.animate()
+    canvas.style.border = '0px solid #000000'
+    animate()
   }
 
   handleClick(e) {
-    ctx.fillRect(e.clientX, e.clientY, 100, 100)
+    console.log(x,xScalar,dx,y,yScalar,dy)
   }
+  
   render() {
     return (
       <div>
         Canvas here
         <canvas 
           ref="canvas" 
-          width={this.state.width} 
-          height={this.state.height} 
+          width={0}
+          height={0}
           onClick={e => this.handleClick(e)}
           style={{
             zIndex: -1,
