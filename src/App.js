728ef22d5306aa65bdf152376f8e8300b923b892
Oliver Shi <oys4cv@virginia.edu>
2019-06-24 23:44:46 -0400

    progress? progress

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 import "./App.css"
 
 const baseImgUrl = "/images"
-// image url, title, link, date(?), description
+// image url, title, link, date, description
 const PostPreview = (props) => {
 	return (
 		<div className="post-preview">
-			<Link to={props.link ? props.link : '/'}> 
-				<img className="post-preview-img" src={baseImgUrl + "/" + props.imgUrl}></img>
+			<Link to={props.link ? props.link : '/'} className="post-preview-img-container"> 
+				<div className="post-preview-img-background">
+					<img className="post-preview-img" src={baseImgUrl + "/" + props.imgUrl}></img>
+					<div className="post-preview-img-date"> {props.date} </div>
+					<div className="post-preview-img-text"> Read More </div>
+				</div>
 			</Link>
 			<div className="desc-container">
-				<Link to={props.link ? props.link : '/'} className="preview-title"> {props.title} </Link>
-				<div className="preview-desc"> {props.desc} </div>
+				<Link to={props.link ? props.link : '/'} className="preview-title-link">
+					<div className="preview-title"> {props.title} </div>
+				</Link>
+				<Link to={props.link ? props.link : '/'} className="preview-desc-link">
+					<div className="preview-desc"> {props.desc} </div>
+				</Link>
 			</div>
+
 		</div>
 	)
 }
@@ -22,12 +31,12 @@ const Home = () => {
 	return (
 			<ul className="post-preview-list">
 				<li>
-					<PostPreview title="title1" desc="post numbrr 1" imgUrl="1.jpg" date="Novemeber whenever" link="1"/>
+					<PostPreview title="title1" date="novemeber whenever" desc="post numbrr 1" imgUrl="1.jpg" date="Novemeber whenever" link="1"/>
 				</li>
 				<li>				
-					<PostPreview title="title2" desc="My 7-Year Pursuit of How To Live A near-death experience, an improbable disorder, and a global quest for answers Dakar,  " imgUrl="2.jpg" link="2" />
+					<PostPreview title="title2" date="fun time" desc="My 7-Year Pursuit of How To Live A near-death experience, an improbable disorder, and a global quest for answers Dakar,  " imgUrl="2.jpg" link="2" />
 				</li><li>				
-					<PostPreview title="title3" desc="lorem eisdfaasdf epsom salt  " imgUrl="piano-beluga.gif" link="2" />
+					<PostPreview title="title3" date="wheeee a watch" desc="lorem eisdfaasdf epsom salt" imgUrl="piano-beluga.gif" link="2" />
 				</li>
 			</ul>
 	)
