3a51551ff7123ac03bd047e0e716cdd4fbf82c49
Oliver Shi <oys4cv@virginia.edu>
2019-06-27 21:20:37 -0400

    sheet music bootstrap

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path');
+const serveIndex = require('serve-index')
 const app = express()
 const port = 3000
 
 app.use('/dist', express.static('dist'))
 app.use('/images', express.static('images'))
+app.use('/sheets', express.static('sheets'), serveIndex('sheets', {'icons': true}))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'))
