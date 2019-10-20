8044dd1cc677b8dda72ec468a45483e66d500dd6
Oliver Shi <oys4cv@virginia.edu>
2019-10-19 22:38:51 -0400

    commit -am sucks

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
+import './home/Home.css'
+
+import Home from './home/Home'
+import About from './about/About'
+import Sheets from './sheets/Sheets'
+
+const Navbar = () => (
+	<nav className='navbar'>
+		<Link className='logo' to='/'> 
+			<i className='material-icons home-icon'> home </i> 
+			<div className='home-text'> Lost in the 21st Century </div>
+		</Link>
+		<ul className='main-nav'>
+			<li>
+				<Link to='/about'> About </Link>
+			</li>
+			<li>
+				<Link to='/sheets'> Sheets </Link>
+			</li>
+		</ul>
+	</nav>
+)
+
+const App = () => (
+	<Router>
+		<Navbar />
+		<div className='container-grid'>
+			<Switch>
+				<Route path='/about' component={About}/>
+				<Route path='/home' component={Home}/>
+				<Route path='/sheets' component={Sheets}/>
+				<Route>
+					<Redirect to='/home/'/>
+				</Route>
+			</Switch>
+		</div>
+	</Router>
+)
+
+export default App;
---
+++ src/components/PostPreview.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Image from '../components/Image'
+import '../home/Home.css'
+
+const baseImgUrl = '/public/images'
+// image url, title, link, date, description
+const PostPreview = ({ link, desc, date, imgUrl, title }) => {
+	return (
+		<div className='post-preview'>
+			<Link to={link ? link : '/'} className='post-preview-img-container'> 
+				<div className='post-preview-img-background'>
+					<Image className='post-preview-img' src={imgUrl}></Image>
+					<div className='post-preview-img-date'> {date} </div>
+					<div className='post-preview-img-text'> Read More </div>
+				</div>
+			</Link>
+			<div className='desc-container'>
+				<Link to={link ? link : '/'} className='preview-title-link'>
+					<div className='preview-title'> {title} </div>
+				</Link>
+				<Link to={link ? link : '/'} className='preview-desc-link'>
+					<div className='preview-desc'> {desc} </div>
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default PostPreview;
