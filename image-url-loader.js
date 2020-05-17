fff7bada384323c26c7b5123a00f4af7cbd0afd3
Oliver Shi <shishishi123@gmail.com>
2020-05-16 22:03:54 -0400

    fix image-url for windows, add padding to breadcrumbs

--- image-url-loader.js
+++ image-url-loader.js
@@ -4,7 +4,10 @@
 module.exports = function () {
   // e.g. /Users/oshi/olivershi.io/docs/public/images/1.jpg
   const resourcePath = this.resourcePath
-  const i = resourcePath.indexOf('public/images/');
+  let i = resourcePath.indexOf('public/images/');
+  if (i === -1) {
+    i = resourcePath.indexOf('public\\images\\')
+  }
   return `module.exports = ${ JSON.stringify({
     url: resourcePath.substring(i)
   }) }`;
