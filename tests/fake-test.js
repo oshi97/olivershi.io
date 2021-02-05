fdc84566697edf261d977451f5a0ef342e7018ed
Oliver Shi <oshi@yext.com>
2021-02-05 17:58:55 -0500

    more fake coverage

--- tests/fake-test.js
+++ tests/fake-test.js
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import Icon from '../src/components/Icon'
 import React from 'React'
+import routes from '../app/routes'
 
 describe('fake test for coverage', () => {
   it('tests stuff', () => {
@@ -10,5 +11,9 @@ describe('fake test for coverage', () => {
   it('can render an icon', () => {
     const { container } = render(<Icon />)
     expect(container).toBeTruthy()
+  })
+
+  it('has routes', () => {
+    expect(Object.keys(routes)).toHaveLength(5)
   })
 })
