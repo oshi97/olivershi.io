387712cf53f41aef3d509dd793412ca91102ead0
Oliver <shishishi123@gmail.com>
2022-07-15 15:10:03 -0400

    add icons back in

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,28 @@
-const validIcons = ['face-black', 'home-black', 'library_music-black', 'videogame_asset-black', 'face-black']
+import { ReactComponent as faceIcon } from '../icons/face-black-24dp.svg'
+import { ReactComponent as homeIcon } from '../icons/home-black-24dp.svg'
+import { ReactComponent as musicIcon } from '../icons/library_music-black-24dp.svg'
+import { ReactComponent as gameIcon } from '../icons/videogame_asset-black-24dp.svg'
+
+const validIcons = {
+  'face-black': faceIcon,
+  'home-black': homeIcon,
+  'library_music-black': musicIcon,
+  'videogame_asset-black': gameIcon
+} as const
+
 const Icon = ({ type, size = 24 }: {
   type: string,
   size?: number
 }) => {
-  if (!type || validIcons.indexOf(type) === -1) {
+  if (!type || !(type in validIcons)) {
     console.warn(`Icon type: ${type} not found in ${validIcons}`)
     return <div></div>
   }
-  const svg = require(`../icons/${type}-${size}dp.svg`).default
-  const markup = {
-    __html: svg
-  }
-  return <div className={'Icon Icon-' + type + '-' + size} dangerouslySetInnerHTML={markup}></div>
+  const Icon = validIcons[type as keyof typeof validIcons]
+  return (
+    <div className={'Icon Icon-' + type + '-' + size}>
+      <Icon/>
+    </div>
+  )
 }
 export default Icon
